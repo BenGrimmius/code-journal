c72d517088e2d290f4d8a4d52333a1704ee2ede2
Ben Grimmius <bengrimmius@gmail.com>
2022-10-28 04:09:31 +0000

    User can create an entry #1

--- js/main.js
+++ js/main.js
@@ -1,8 +1,6 @@
 var $photoInput = document.querySelector('.photo-input');
-// console.log($photoInput);
 
 var $img = document.querySelector('img');
-// console.log($img);
 
 $photoInput.addEventListener('input', function (event) {
   $img.attributes.src.textContent = event.target.value;
