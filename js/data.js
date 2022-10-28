96884d559e43d8b879d2f6240da169ebaf57a308
Ben Grimmius <bengrimmius@gmail.com>
2022-10-28 04:08:27 +0000

    User can create an entry #1

--- js/data.js
+++ js/data.js
@@ -6,3 +6,14 @@ var data = {
   editing: null,
   nextEntryId: 1
 };
+
+var previousDataJSON = localStorage.getItem('CODE-JOURNAL');
+if (previousDataJSON !== null) {
+  data = JSON.parse(previousDataJSON);
+}
+
+window.addEventListener('beforeunload', function (event) {
+  var dataJSON = JSON.stringify(data);
+  this.localStorage.setItem('CODE-JOURNAL', dataJSON);
+  data.nextEntryId++;
+});
--- js/main.js
+++ js/main.js
@@ -7,3 +7,18 @@ var $img = document.querySelector('img');
 $photoInput.addEventListener('input', function (event) {
   $img.attributes.src.textContent = event.target.value;
 });
+
+var $form = document.querySelector('form');
+
+$form.addEventListener('submit', function (event) {
+  event.preventDefault();
+  var submission = {};
+  submission.title = $form[0].value;
+  submission.photoURL = $form[1].value;
+  submission.notes = $form[2].value;
+  submission.EntryID = data.nextEntryId;
+  data.nextEntryId++;
+  data.entries.unshift(submission);
+  this.reset();
+  $img.setAttribute('src', 'images/placeholder-image-square.jpg');
+});
