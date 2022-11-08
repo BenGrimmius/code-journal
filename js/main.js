efbaa984f87d0fe109081669eae589f51f7dfffb
Ben Grimmius <bengrimmius@gmail.com>
2022-11-08 03:58:43 +0000

    Complete feature 2

--- js/main.js
+++ js/main.js
@@ -29,4 +29,45 @@ $form.addEventListener('submit', function (event) {
   data.entries.unshift(submission);
   this.reset();
   $img.setAttribute('src', 'images/placeholder-image-square.jpg');
+  var newEntry = renderEntries(submission);
+  $entryList.prepend(newEntry);
+});
+
+function renderEntries(entry) {
+
+  var li = document.createElement('li');
+
+  var rowOneDiv = document.createElement('div');
+  rowOneDiv.setAttribute('class', 'row');
+
+  var img = document.createElement('img');
+  img.setAttribute('class', 'column-half');
+  img.setAttribute('src', entry.photoURL);
+
+  var colHalfDiv = document.createElement('div');
+  colHalfDiv.setAttribute('class', 'column-half');
+
+  var hThree = document.createElement('h3');
+  hThree.textContent = entry.title;
+
+  var p = document.createElement('p');
+  p.textContent = entry.notes;
+
+  li.appendChild(rowOneDiv);
+  rowOneDiv.appendChild(img);
+  rowOneDiv.appendChild(colHalfDiv);
+  colHalfDiv.appendChild(hThree);
+  colHalfDiv.appendChild(p);
+
+  return li;
+
+}
+
+var $entryList = document.querySelector('ul');
+
+window.addEventListener('DOMContentLoaded', function (event) {
+  for (var i = 0; i < data.entries.length; i++) {
+    var list = renderEntries(data.entries[i]);
+    $entryList.append(list);
+  }
 });
